c277212631f475cf1df7213d5f0f23d9777f1cd3
Denis Fedash <0501054104d@gmail.com>
2021-12-09 12:22:05 +0200

    add

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleyEl = document.querySelector('.gallery');
+const cardMrakup = createImageCards(galleryItems);
+
+galleyEl.insertAdjacentHTML('beforeend', cardMrakup);
+galleyEl.addEventListener('click', onGalleryClick);
+function createImageCards(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="Image ${description}"
+    />
+  </a>
+</div>`
+    }).join(' ');
+};
+
+const imgEl = e.target;
+const bigImgEl = imgEl.closest('[data-source="${original}"]');
+console.log(bigImgEl);
+
