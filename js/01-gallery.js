ca848da55803463a79be59df0e92c92c03c58688
Denis Fedash <0501054104d@gmail.com>
2021-12-10 12:44:33 +0200

    Внес корректировки в 1 задачу

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,15 +1,10 @@
 import { galleryItems } from './gallery-items.js';
 // // // Change code below this line
 
-const instance = basicLightbox.create(`
-  <div class="modal">	
-    <img src= ' ' />      
-  </div>
-`);
+
 
 const galleryItemsEl = document.querySelector('.gallery');
 const cardsGallery = createImageCards(galleryItems);
-const image = instance.element().querySelector('img');
 
 galleryItemsEl.insertAdjacentHTML('beforeend', cardsGallery);
 galleryItemsEl.addEventListener('click', onPaletteContainer);
@@ -17,7 +12,7 @@ function createImageCards(galleryItems) {
   return galleryItems.map(({ preview, original, description }) => {
       
     return `<div class="gallery__item">
-      <a class="gallery__link" href="${original}" data-lightBox = "image-group">
+      <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
       src="${preview}"
@@ -30,12 +25,16 @@ function createImageCards(galleryItems) {
 };
 function onPaletteContainer(evt) {
   evt.preventDefault();
-  image.src = `original`;
-  instance.show();
+  
   if (!evt.target.classList.contains('gallery__image') ) {
     return;
   }
   console.log(evt.target);
+  const instance = basicLightbox.create(`  	
+    <img src= "${evt.target.dataset.source}" /> 
+`);
+  instance.show();
+  
 };
 
 
